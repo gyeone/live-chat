be8ec4141e8d269ddc2949c0a2f30be25d1ff4e4
gyeone <voov000@naver.com>
2026-05-08 22:52:04 +0900

    login.jsx 닉네임 입력란 기능 구현

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 function Login() {
+    const [nickname, setNickname] = useState("");
     return (
-        <>
+        <section className="login">
             <h2>닉네임을 입력해주세요</h2>
-            <form id="form" action="">
-                <input id="nickname" type="text" />
-                <button id="login-btn">로그인</button>
+            <form id="nickname-form" onSubmit={handleSubmit}>
+                <input
+                    id="nickname"
+                    type="text"
+                    value={nickname}
+                    maxLength={10}
+                    onChange={(e) => setNickname(e.target.value.trim())}
+                    placeholder="10자 이내의 닉네임을 입력하세요"
+                />
+                <button type="reset">x</button>
+                <button>입장</button>
             </form>
-        </>
+        </section>
     );
 }
 
