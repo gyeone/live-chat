8ac5668a04f8f778bf040dc387fa4158364229e1
gyeone <voov000@naver.com>
2026-05-08 23:00:32 +0900

    chatList.jsx 소켓 연결, 닉네임이 담긴 로그인 입장 메시지 남기기

--- src/pages/chatList.jsx
+++ src/pages/chatList.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import socket from "../socket";
+
 function ChatList() {
+    const [nickname, SetNickname] = useState("");
+
+    useEffect(() => {
+        socket.on("welcome", (nickname) => {
+            SetNickname(nickname);
+        });
+        return () => {
+            socket.off("welcome");
+        };
+    }, []);
     return (
         <>
-            <ul></ul>
+            <section id="chat-list">
+                <div className="chatList-header">
+                    <h2>{nickname}님 반갑습니다!</h2>
+                </div>
+                <ul></ul>
+            </section>
         </>
     );
 }
