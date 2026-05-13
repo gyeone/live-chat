6cdc6292288835106ad441afd3f646650a146103
gyeone <voov000@naver.com>
2026-05-13 21:37:04 +0900

    chatList.jsx, createRoom.jsx 방 만들기 버튼 클릭 시 모달 열고 닫기(닫는 props 넘겨주기)기능 구현

--- src/components/createRoom.jsx
+++ src/components/createRoom.jsx
@@ -1,19 +1,25 @@
-function CreateRoom() {
+import { useEffect, useState } from "react";
+function CreateRoom({ onClose }) {
     return (
-        <dialog>
-            <h3>방 만들기</h3>
-            <form id="roomName-form" onSubmit={handleSubmit}>
-                <input
-                    id="roomName"
-                    type="text"
-                    maxLength={50}
-                    value={roomName}
-                    onChange={(e) => setRoomName(e.target.value.trim())}
-                    placeholder="50자 이내의 방 이름을 입력하세요"
-                />
-                <button>완료</button>
-            </form>
-        </dialog>
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <h3>방 만들기</h3>
+                <form id="roomName-form" onSubmit={handleSubmit}>
+                    <input
+                        id="roomName"
+                        type="text"
+                        maxLength={50}
+                        value={roomName}
+                        onChange={(e) => setRoomName(e.target.value.trim())}
+                        placeholder="50자 이내의 방 이름을 입력하세요"
+                    />
+                    <button>완료</button>
+                    <button type="button" onClick={onClose}>
+                        닫기
+                    </button>
+                </form>
+            </div>
+        </div>
     );
 }
 
--- src/pages/chatList.jsx
+++ src/pages/chatList.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import socket from "../socket";
+import CreateRoom from "../components/createRoom";
 import { useNavigate } from "react-router-dom";
 
 function ChatList() {
     const [nickname, SetNickname] = useState("");
     const [count, setCount] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -30,9 +33,15 @@ function ChatList() {
                 <div className="chatList-header">
                     <h2>{nickname}님 반갑습니다!</h2>
                     <h2>현재 접속자 수 {count}명</h2>
+                    <button type="button" onClick={() => setIsModalOpen(true)}>
+                        + 방 만들기
+                    </button>
                 </div>
                 <ul></ul>
             </section>
+            {isModalOpen && (
+                <CreateRoom onClose={() => setIsModalOpen(false)} />
+            )}
         </>
     );
 }
