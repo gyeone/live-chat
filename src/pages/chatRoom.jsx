ba4ea8e5d698aaff9228a93a2a112b21bb369e16
gyeone <voov000@naver.com>
2026-05-15 01:34:38 +0900

    chatRoom.jsx 메시지 입력란과 전송버튼 기능 구현, 전송 버튼 클릭 시 메시지 데이터 db에 보내기

--- src/pages/chatRoom.jsx
+++ src/pages/chatRoom.jsx
@@ -1,8 +1,12 @@
+import socket from "../socket";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+
+function ChatRoom() {
     const [nickname, SetNickname] = useState("");
+    const [inputMsg, setInputMsg] = useState("");
     const [roomName, setRoomName] = useState("");
 
     const navigate = useNavigate();
@@ -23,6 +27,15 @@ import { useEffect } from "react";
         navigate("/chatList");
         window.location.reload();
     };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (inputMsg) {
+            socket.emit("chat message", { inputMsg, roomName, nickname });
+            setInputMsg("");
+        }
+    };
+
     return (
         <>
             <section id="chatRoom">
@@ -30,7 +43,18 @@ import { useEffect } from "react";
                     <button type="button" onClick={toPrevPage}>
                         이전
                     </button>
+                    <h2>{roomName}</h2>
                 </div>
+                <form id="inputMsg-form" onSubmit={handleSubmit}>
+                    <input
+                        id="input"
+                        type="text"
+                        value={inputMsg}
+                        autoComplete="off"
+                        onChange={(e) => setInputMsg(e.target.value)}
+                    />
+                    <button>전송</button>
+                </form>
             </section>
         </>
     );
