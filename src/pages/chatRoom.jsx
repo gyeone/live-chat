e67dfab087d1a70ad660e557f87cb61c5d77487e
gyeone <voov000@naver.com>
2026-05-15 01:22:59 +0900

    chatRoom.jsx chatList에서 useNavigate으로 유저가 클릭한 roomName(state)을 useLocation 이용해 받기

--- src/pages/chatRoom.jsx
+++ src/pages/chatRoom.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
     const [nickname, SetNickname] = useState("");
+    const [roomName, setRoomName] = useState("");
+
     const navigate = useNavigate();
+    const { state } = useLocation();
+
     useEffect(() => {
-        const nickname = socket.nickname || sessionStorage.getItem("nickname");
+        const nickname = sessionStorage.getItem("nickname");
         SetNickname(nickname);
+        setRoomName(state);
 
         if (!nickname) {
             navigate("/");
