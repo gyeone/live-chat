a93d11375ebc9862b8b73206223f61f4fa37cceb
gyeone <voov000@naver.com>
2026-05-29 01:26:30 +0900

    myPage.jsx 닉네임 세션스토리지에서 가져와 보여주기

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
+
 function MyPage() {
+    const [nickname, setNickname] = useState("");
+
+    useEffect(() => {
+        const nickname = sessionStorage.getItem("nickname");
+        setNickname(nickname);
+    });
     return (
         <section className="myPage">
+            <h2>{nickname}</h2>
         </section>
     );
 }
