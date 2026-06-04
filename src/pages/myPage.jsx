f8ce5909bbef822191c26c9986d5bd106ed22cf1
gyeone <voov000@naver.com>
2026-06-04 21:03:55 +0900

    myPage.jsx 마이페이지 이전 페이지로 이동 버튼 기능 구현

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function MyPage() {
     const [nickname, setNickname] = useState("");
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const nickname = sessionStorage.getItem("nickname");
         setNickname(nickname);
     });
+
+    const toPrevPage = () => {
+        navigate("/chatList");
+        window.location.reload();
+    };
     return (
         <section className="myPage">
+            <button type="button" onClick={toPrevPage}>
+                이전
+            </button>
             <h2>{nickname}</h2>
         </section>
     );
