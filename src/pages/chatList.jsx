a62161d8f01791fda846f71db9f2167a6eecafa0
gyeone <voov000@naver.com>
2026-06-07 00:03:00 +0900

    chatList.jsx state 이름 직관적으로 변경, html 구조 변경

--- src/pages/chatList.jsx
+++ src/pages/chatList.jsx
@@ -10,7 +10,7 @@ function ChatList() {
     const [userList, setUserList] = useState([]);
     const [messages, setMessages] = useState([]);
     const [rooms, setRooms] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
     const [selectRoom, setSelectRoom] = useState("");
     const [isChatRoomOpen, setIsChatRoomOpen] = useState(false);
 
@@ -65,42 +65,43 @@ function ChatList() {
                                 <li key={i}>{user.nickname}</li>
                             ))}
                     </ul>
-                    <button type="button" onClick={() => setIsModalOpen(true)}>
+                    <button
+                        type="button"
+                        onClick={() => setIsCreateModalOpen(true)}
+                    >
                         + 방 만들기
                     </button>
-                    <div className="chatList-rooms">
-                        <ul className="rooms">
-                            {rooms.map((room, i) => (
-                                <li
-                                    className="room"
-                                    key={i}
-                                    onClick={() =>
-                                        handleChatRoom(room.room_name)
-                                    }
-                                >
-                                    <p>{room.room_name}</p>
-                                    <p>
-                                        {room.roomContent?.content
-                                            ? room.roomContent?.content
-                                            : "채팅 내용이 없습니다"}
-                                    </p>
-                                    <p>{room.roomContent?.created_at}</p>
-                                </li>
-                            ))}
-                            {rooms.length === 0 && <p>생성된 방이 없습니다.</p>}
-                        </ul>
-                    </div>
                 </div>
-                <ul></ul>
+                <div className="chatList-rooms">
+                    <ul className="rooms">
+                        {rooms.map((room, i) => (
+                            <li
+                                className="room"
+                                key={i}
+                                onClick={() => handleChatRoom(room.room_name)}
+                            >
+                                <p>{room.room_name}</p>
+                                <p>
+                                    {room.roomContent?.content
+                                        ? room.roomContent?.content
+                                        : "채팅 내용이 없습니다"}
+                                </p>
+                                <p>{room.roomContent?.created_at}</p>
+                            </li>
+                        ))}
+                        {rooms.length === 0 && <p>생성된 방이 없습니다.</p>}
+                    </ul>
+                </div>
             </section>
-            {isModalOpen && (
-                <CreateRoom onClose={() => setIsModalOpen(false)} />
+            {isCreateModalOpen && (
+                <CreateRoom onClose={() => setIsCreateModalOpen(false)} />
+            )}
             )}
             {selectRoom && isChatRoomOpen && (
                 <ChatRoom
                     nickname={nickname}
                     roomName={selectRoom}
-                    setIsChatRoomOpen={setIsChatRoomOpen}
+                    setIsChatRoomOpen={() => setIsChatRoomOpen(false)}
                 />
             )}
         </>
