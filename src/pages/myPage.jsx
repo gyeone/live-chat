d2e3107ea6b6b2e8a0f971489adb2e63b4712493
gyeone <voov000@naver.com>
2026-06-06 21:46:18 +0900

    myPage.jsx db에서 user_profile 값 가져오기, 사용자가 설정한 이미지가 없을 경우 기본 이미지로 지정

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import defaultImg from "../assets/images/default_img.png";
 
 function MyPage() {
     const [nickname, setNickname] = useState("");
+    const [profile, setProfile] = useState();
 
     const navigate = useNavigate();
 
     useEffect(() => {
         const nickname = sessionStorage.getItem("nickname");
+
         setNickname(nickname);
-    });
+        socket.emit("get profile img", nickname);
 
         if (!nickname) {
             navigate("/");
             return;
         }
+
+        socket.on("profile img", (imgUrl) => {
+            setProfile(`http://localhost:3000${imgUrl}`);
+        });
+
+        return () => {
+            socket.off("profile img");
+        };
+    }, []);
 
     const toPrevPage = () => {
         navigate("/chatList");
@@ -26,6 +38,10 @@ function MyPage() {
                 이전
             </button>
             <h2>{nickname}</h2>
+
+            <div>
+                <img src={profile ? profile : defaultImg} alt="프로필사진" />
+            </div>
         </section>
     );
 }
