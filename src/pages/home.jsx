a032c632a0411dd37756dd93bcc04da8b7f15d32
gyeone <voov000@naver.com>
2026-06-10 01:36:59 +0900

    home.jsx 홈페이지에 인사말 json파일 생성 후 랜덤으로 여러개 보여주기

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ChatList from "../components/chatList";
+import comment from "../data/homeComment.json";
 
 function Home() {
     const [nickname, setNickname] = useState("");
-
+    const [commentId, setCommentId] = useState(1);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -12,17 +13,23 @@ function Home() {
         setNickname(nickname);
 
         if (!nickname) {
-            navigate("/");
+            navigate("/login");
             return;
         }
+
+        const random = Math.floor(Math.random() * 10);
+        setCommentId(random);
     }, []);
 
     return (
         <>
-            <section id="home">
-                <h2>{nickname}님 반갑습니다!</h2>
+            <section className="home">
+                <ChatList />
+                <div className="home-contents">
+                    <h2>{nickname}님 반갑습니다!</h2>
+                    <p>{comment[commentId].comment}</p>
+                </div>
             </section>
-            <ChatList />
         </>
     );
 }
